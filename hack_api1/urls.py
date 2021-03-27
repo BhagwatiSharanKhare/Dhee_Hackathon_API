af7689b0257666e7ac4e1e53628aff30e9758901
Bhagwati <bhagwatisharankhare@gmail.com>
2021-03-27 13:26:46 +0530

    Update urls.py

--- hack_api1/urls.py
+++ hack_api1/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('sub_api.urls'))
+    path('api/', include('weather_api.urls'))
 ]
